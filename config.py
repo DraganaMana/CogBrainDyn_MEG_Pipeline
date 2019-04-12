30a235e56c759d25f0dd8477dad37ec23d9067c0
DraganaMana <dragana.manasova@gmail.com>
2019-04-12 15:10:49 +0200

    small changes

--- config.py
+++ config.py
@@ -4,79 +4,7 @@ Config file
 ===========
 
 Configuration parameters for the study. This should be in a folder called
-``library/`` inside the ``processing/`` directory.
-
-Study Specific Parameters:
-    
-    plot : if True, the single scripts generate plots
-    
-    # DIRECTORIES #
-    study_path : path to the directory that contains your data
-    subjects_dir : path pointing to the antomical files for all subjects
-    meg_dir : path pointing to the MEG files for all subjects
-    
-    # SUBJECTS / RUNS #
-    study_name : the name of your experiment
-    subjects : a list of the subject names
-    exclude_subjects : the list of subjects to exclude from the above
-    runs : list of strings that specifies how your runs are named. 
-            For example runs = ['run01', 'run02']. If you have only one run 
-            it should be runs = [''].
-    base_raw_fname : string that describes how your files are named. 
-            For example: '{subject}_audvis_{run}_raw.fif'
-            
-    # BAD CHANNELS #
-    bads : to be removed before maxfilter is applied (defined per run)
-    
-    # DEFINE ADDITIONAL CHANNELS #
-    set_channel_types : set channel type of extra channels that were recorded 
-            (e.g. EOG, ECG etc.) Example: set type for EEG062 as EOG.
-    rename_channels : rename channels. Example: rename channel EEG062 to EOG062.
-    bads : dictionary containing he list of bad channels for each subject
-    
-    # FREQUENCY FILTERING #
-    h_freq : the high-frequency cut-off in the lowpass filtering step. 
-            Keep it None if no lowpass filtering should be applied.
-    l_freq : the low-frequency cut-off in the highpass filtering step. 
-            Keep it None if no highpass filtering should be applied.
-            
-    # MAXFILTER PARAMETERS #
-    mf_ctc_fname : for maxfiltering, path to the cross talk file on this machine.
-    mf_cal_fname : for maxiltering, path to the calibration file on this machine.
-    mf_reference_run : specify which run to use for HPI recalibration, all other 
-            runs will have head position adjusted to this one.
-    mf_head_origin : The origin of the head used for maxwell filtering.
-    mf_st_duration : a float that specifies the buffer duration in seconds. 
-            As an example, 10s acts like a 0.1 Hz highpass filter. 
-            If None (default), no temporal spatial filtering is applied 
-            during MaxFilter.
-            
-    # RESAMPLING #
-    resample_sfreq : a float that specifies at which sampling frequency the data 
-            should be resampled. 
-            If None then no resampling will be done.
-    decim : integer that says how much to decimate data at the epochs level. 
-            It is typically an alternative to the resample_sfreq parameter.
-            
-    # AUTOMATIC REJECTION OF ARTIFACTS #
-    reject : the default rejection limits to make some epochs as bads. 
-            This allows to remove strong transient artifacts. 
-            Note: these numbers tend to vary between subjects.
-            
-    # EPOCHING #
-    tmin: float that gives the start time before event of an epoch.
-    tmax : float that gives the end time after event of an epochs.
-    baseline : tuple that specifies how to baseline the epochs. 
-            If None, then no baseline applied.
-    event_id : python dictionary that maps events (trigger/marker values) to 
-            conditions. E.g. event_id = {'Auditory/Left': 1, 'Auditory/Right': 2}
-    runica : boolean that says if ICA should be used or not.
-    
-    # ICA PARAMETERS #
-    runica : boolean that says if ICA should be used or not.
-    
-
-    
+``library/`` inside the ``processing/`` directory. 
 
 """
 
@@ -86,13 +14,10 @@ from mne.datasets import sample
 
 
 # let the scripts generate plots or not
+plot = True
+
 # execute %matplotlib qt in your command line once to show the figures in
 # separate windows
-
-plot = True
-# execute %matplotlib qt 
-# in the command line to get the plots in extra windows
-
 ###############################################################################
 # DIRECTORIES
 # -----------
@@ -142,8 +67,8 @@ eeg = False # True
 # This generates the name for all files
 # with the names specified above
 # normally you should not have to touch this
-base_fname = '{subject}_' + study_name + '{extension}.fif'
-
+base_fname = '{subject}_' + study_name + '{extension}.fif'            
+            
 ###############################################################################
 # BAD CHANNELS
 # ------------
