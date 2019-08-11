c2db98aab3dc2a32ed2d66e82a42386fa9ef84f6
DraganaMana <32870328+DraganaMana@users.noreply.github.com>
2019-08-11 17:39:04 +0200

    changes to crop_RS.py

--- crop_RS.py
+++ crop_RS.py
@@ -8,6 +8,7 @@ Created on Sun Jul 28 12:32:29 2019
 import os.path as op
 
 import mne
+import numpy as np
 
 #import config
 
@@ -16,16 +17,18 @@ subjects_list = ['hm070076', 'fr190151', 'at140305', 'cc150418', 'eb180237', 'ld
                  'tr180110', 'ep190335', 'gl180335', 'lr190095', 'ad190325', 'ag170045', 
                  'pl170230', 'ma190185'] 
 
+#subjects_list = ['hm070076']
+
 RSs = ['RS01', 'RS02']
 
 study_name = 'ScaledTime'
 
 # Directory where the .fif files are
-meg_dir = '...'
+meg_dir = 'D:/ScaledTime/MEGdata/MEG/'
 
 stim_channel = 'STI101' 
-min_event_duration = 0.005
-shortest_event = 1
+min_event_duration = 0.0001
+shortest_event = 0
 freq = 2000
 
 for subject in subjects_list:
@@ -41,6 +44,8 @@ for subject in subjects_list:
         raw = mne.io.read_raw_fif(raw_fname_in,
                                   allow_maxshield=True,
                                   preload=True, verbose='error')
+#        raw.plot(n_channels=50, butterfly=False, group_by='original')
+
         
         # Find the events
         events = mne.find_events(raw, stim_channel=stim_channel,
@@ -55,10 +60,43 @@ for subject in subjects_list:
         # I tried the code w/downsampled data so the events were with a slighly different length.
         # But this should be fine with the Multifracs data which is not resampled. 
         
+        # Keep only the triggers with value 1.        
+        events = events[events[:, 2] == 1]
+
+        
         # Crop the data between the two triggers - at the beginning of the 5min interval, 
         # and at the end.
-        RS_crop = mne.io.Raw.crop(mne.io.Raw.copy(raw), 
-                                  tmin=events[0][0]/freq, 
-                                  tmax=(events[1][0]/freq))
+        row, col = events.shape
+        
+        # EXCEPTION for subject 'at140305' and similar cases
+        event_time = events[0][0]/freq
+        raw_length = raw.n_times/freq
+        if raw_length < event_time:
+            print("WARNING: The length of the raw is shorter than the event timestamp.")
+            tmax = raw_length - 1 # minus 1 sec to compensate for rounding up
+            tmin = raw_length - 301
+            RS_crop = mne.io.Raw.crop(mne.io.Raw.copy(raw), 
+                                      tmin=tmin, 
+                                      tmax=tmax)
+        # For all other subjects
+        elif raw_length > event_time:
+            if row == 1: # If only one event is found, set the crop values +/- 300sec 
+                         # from that trigger. 
+                print("WARNING: Only one trigger is found.")
+                event_time = events[0][0]/freq
+                if event_time >= 300:
+                    tmin = event_time - 300
+                    tmax = event_time
+                elif event_time < 300:
+                    tmin = event_time
+                    tmax = event_time + 300
+                RS_crop = mne.io.Raw.crop(mne.io.Raw.copy(raw), 
+                                          tmin=tmin, 
+                                          tmax=tmax)
+            elif row == 2:
+                RS_crop = mne.io.Raw.crop(mne.io.Raw.copy(raw), 
+                                          tmin=events[0][0]/freq, 
+                                          tmax=(events[1][0]/freq))
+            
         RS_fname_out = op.join(meg_subject_dir, RS + '_cropped.fif')
         RS_crop.save(RS_fname_out, overwrite=True)
